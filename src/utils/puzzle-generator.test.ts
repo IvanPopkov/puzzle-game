8a2818c20d409e9a484853ae39087c86e44335a6
IvanPopkov <ivan.d.popkov@gmail.com>
2025-03-11 00:25:07 +0100

    feat: add puzzle generator test

--- src/utils/puzzle-generator.test.ts
+++ src/utils/puzzle-generator.test.ts
@@ -1,9 +1,10 @@
-import {describe, expect, it} from "vitest";
+import {beforeEach, describe, expect, it} from "vitest";
 import {verifyEligibility} from "./puzzle-generator.ts";
 
 describe('Puzzle Generator', () => {
-  it('solution should be eligible', () => {
-    const puzzle = [
+  let puzzle: (number | '')[];
+  beforeEach(() => {
+    puzzle = [
       1, 2, 3, 4, 5, 6, 7, 8, 9,
       2, 3, 4, 5, 6, 7, 8, 9, 0,
       3, 4, 5, 6, 7, 8, 9, 1, 2,
@@ -14,11 +15,20 @@ describe('Puzzle Generator', () => {
       8, 9, 1, 2, 3, 4, 5, 6, 7,
       9, 1, 2, 3, 4, 5, 6, 7, 8
     ];
-    const candidate = 1;
-    const index = 0;
+  });
 
-    const result = verifyEligibility(puzzle, candidate, index);
+  [
+    { title: 'solution should be eligible', candidate: 1, setup: () => {}, expected: [] },
+    { title: 'should not have column violation', candidate: 2, setup: () => puzzle[1] = '', expected: ['column'] },
+    { title: 'should not have row violation', candidate: 2, setup: () => puzzle[9] = '', expected: ['row'] },
+  ].forEach(testCase => {
+    it(testCase.title, () => {
+      testCase.setup();
+      const index = 0;
 
-    expect(result.length).toBe(0);
-  })
+      const result = verifyEligibility(puzzle, testCase.candidate, index);
+
+      expect(result).toStrictEqual(testCase.expected);
+    });
+  });
 });
