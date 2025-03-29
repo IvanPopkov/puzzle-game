9731dcd7416c4c2b9acd7d4131cb94e6b6c02a27
IvanPopkov <ivan.d.popkov@gmail.com>
2025-03-29 21:25:57 +0100

    fix: test deploy vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  test: {
-
-  },
+  test: {},
   plugins: [react()],
   base: '/puzzle-game/'
 })
