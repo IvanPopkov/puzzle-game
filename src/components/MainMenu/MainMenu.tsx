4735d315ef3d29044ef9c87004f2b15390a1eba6
IvanPopkov <ivan.d.popkov@gmail.com>
2025-08-03 23:54:21 +0200

    fix: add base url

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -4,7 +4,7 @@ import {BASE_URL} from "../../constants.ts";
 const MainMenu = () => {
   return (
     <div className={styles.gamesList}>
-      <a href={`${BASE_URL}#/sudoku`} className="sudoku-game"><img alt={'Play sudoku'} src={'public/sudoku.png'} /></a>
+      <a href={`${BASE_URL}#/sudoku`} className="sudoku-game"><img alt={'Play sudoku'} src={`${import.meta.env.BASE_URL}sudoku.png`} /></a>
       <a href={`${BASE_URL}#/kenken`} className="kenken-game">Play KenKen</a>
     </div>
   );
